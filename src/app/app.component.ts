8a972e270c67ab16126de036b1587bcf64bc4189
Joseph Sun <joseph.sun@tylertech.com>
2025-05-23 17:03:45 -0400

    add Shimmer Hero text effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , NavbarComponent],
+  imports: [RouterOutlet , NavbarComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  title = 'MichaelHatesJava';
 }
