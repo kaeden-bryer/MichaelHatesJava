94d2e7b93315ececb38eb127ebf570f1cedc2365
Kaeden Bryer <kaeden.bryer@tylertech.com>
2025-05-23 14:53:29 -0400

    Add components

---
+++ src/app/code-block/code-block.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-block',
+  imports: [],
+  templateUrl: './code-block.component.html',
+  styleUrl: './code-block.component.css'
+})
+export class CodeBlockComponent {
+
+}
